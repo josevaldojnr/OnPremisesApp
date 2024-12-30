ae34f496df685fb3a3acc134462c649f64e2f842
josevaldojnr <juniorjosevaldo1@gmail.com>
2024-12-29 21:37:04 -0300

    testing cicd

---
+++ deployment/native/styles.js
@@ -0,0 +1,10 @@
+import * as style0 from "C:/Users/josevaldo/Mendix/OnPremisesApp/themesource/administration/native/main";
+import * as style1 from "C:/Users/josevaldo/Mendix/OnPremisesApp/themesource/nanoflowcommons/native/main";
+import * as style2 from "C:/Users/josevaldo/Mendix/OnPremisesApp/themesource/atlas_core/native/main";
+import * as style3 from "C:/Users/josevaldo/Mendix/OnPremisesApp/themesource/feedbackmodule/native/main";
+import * as style4 from "C:/Users/josevaldo/Mendix/OnPremisesApp/themesource/myfirstmodule/native/main";
+import * as style5 from "C:/Users/josevaldo/Mendix/OnPremisesApp/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5]);
